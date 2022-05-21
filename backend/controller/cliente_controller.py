5d04b8db8b5d6d0bef73eca9fe2d1ec83aef9079
Tatiwel <49209072+Tatiwel@users.noreply.github.com>
2022-05-21 20:32:20 -0300

    commit

--- backend/controller/cliente_controller.py
+++ backend/controller/cliente_controller.py
@@ -1,4 +1,8 @@
+from statistics import quantiles
+from unicodedata import category
 from flask import Blueprint, redirect, render_template, request, url_for, session
+from model.cliente import cliente
+from extensions.extensions import db
 
 cliente = Blueprint('cliente', __name__, template_folder='../view', url_prefix='/cliente')
 
@@ -8,10 +12,10 @@ def index():
     if session.get("cliente_result") != None:
         result = session["cliente_result"]
         session.pop("cliente_result")
-    produtos = Produto.query.all()
-    return render_template('cliente/index.html', produtos=produtos, result=result)
+    clientes = Cliente.query.all()
+    return render_template('cliente/index.html', clientes=clientes, result=result)
 
-@cliente.route('/registrar', methods=['GET', 'POST'])
+@cliente.route('/register', methods=['GET', 'POST'])
 def register():
     cliente = Cliente.query.all()
     if request.method == 'GET':
@@ -22,7 +26,7 @@ def register():
             cpf = "",
             cep = None
         )
-        return render_template('produto/register.html', cliente=cliente)
+        return render_template('cliente/register.html', cliente=cliente)
     
     if request.method == 'POST':
         cliente = Cliente(
@@ -51,5 +55,28 @@ def update(id):
         return render_template('cliente/update.html', cliente=cliente_atual)
     else:
         cliente = Cliente(
-            
+            nome = request.form['nome'],
+            telefone = request.form['telefone'],
+            email = request.form['email'],
+            cpf = request.form['cpf'],
+            cep = request.form['cep']
         )
+
+        cliente_atual.fill_update(cliente)
+        result = cliente_atual.is_valid()
+
+        if result.success:
+            db.session.commit()
+            result.message = "Cliente atualizado com sucesso!"
+            session["cliente_result"] = result.to_json()
+            return redirect(url_for('cliente.index'))
+
+        return render_template('cliente/update.html', cliente=cliente_atual, result=result.to_json())
+
+
+@cliente.route('<int:id>/delete', methods=['GET', 'POST'])
+def delete(id):
+    cliente_atual = Cliente.query.get_or_404(id)
+    db.session.add(cliente_atual)
+    db.session.commit()
+    return redirect(url_for('cliente.index'))
---
+++ backend/model/cliente.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column
+from extensions.extensions import db
+from model.shared.result import Result
+
+class Cliente(db.Model):
+    __tablename__ = 'clientes'
+
+    nome = Column(db.String(250), nullable = False)
+    telefone = Column(db.String(11), nullable = False)
+    email = Column(db.String(250), nullable = False)
+    cpf = Column(db.String(11), nullable = False)
+    cep = Column(db.String(8), nullable = False)
+
+
+    def is_valid(self) -> Result:
+        if (
+            not self.nome or len(self.nome) == 0 or
+            not self.telefone or len(self.telefone) == 0 or
+            not self.email or len(self.email) == 0 or
+            not self.cpf or len(self.cpf) == 0 or
+            not self.cep or len(self.cep) == 0
+        ):
+            return Result(success= False, message="Preencha todos os campos!")
+
+        return Result(success=True)
+
+    
+    def fill_update(self, cliente):
+        self.nome = cliente.nome
+        self.telefone = cliente.telefone
+        self.email = cliente.email
+        self.cpf = cliente.cpf
+        self.cep = cliente.cep
