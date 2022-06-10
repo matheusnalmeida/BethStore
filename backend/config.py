a3c2d5f8fcfdcb70a269acb1a0475e9d58b89d72
matheusnalmeida <matheus.nunes00@souunit.com.br>
2022-06-10 01:01:36 -0300

    Old sqllite

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,4 @@ class DefaultConfig:
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/BethStore' if not os.environ.get('DATABASE_URL') else not os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+    #'sqlite:///database.db' 
