f312913f8d1009e898c6ebeaf7c49f83c73e3c59
matheusnalmeida <matheus.nunes00@souunit.com.br>
2022-06-10 00:37:10 -0300

    Adicionando integração com postgreesql

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,9 @@
+import os
+
 class DefaultConfig:
-    SECRET_KEY = "ef280044-fc3b-4205-8978-ae2849a49933"
+    SECRET_KEY = os.environ.get('SECRET_KEY', "ef280044-fc3b-4205-8978-ae2849a49933")
     PROJECT = "Beth Store"
     DEBUG = True
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/BethStore' if not os.environ.get('DATABASE_URL') else not os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
