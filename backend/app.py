306b47e20a20a0911a2ad4ededf9c00cc7b84db8
matheusnalmeida <matheus.nunes00@souunit.com.br>
2022-06-09 23:22:28 -0300

    Remove 404

--- backend/app.py
+++ backend/app.py
@@ -45,11 +45,6 @@ app.register_blueprint(frete)
 
 init_db(app)
 
-# App 404 error handler
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("erros/404.html"), 404
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
