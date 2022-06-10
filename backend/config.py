194950c4392e63e23e54c59e47014400aeb117a0
Matheus Nunes De Almeida <matheus.nunes00@souunit.com.br>
2022-06-10 08:50:40 -0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,5 +6,5 @@ class DefaultConfig:
     DEBUG = True
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/BethStore' if not os.environ.get('DATABASE_URL') else not os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/BethStore' if not os.environ.get('DATABASE_URL') else os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
     #'sqlite:///database.db' 
